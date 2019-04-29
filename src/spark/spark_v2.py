ad05e821fc8806d90ea304e3e20a2dfcb1477a66
Jingguang Zhou <jingguaz@perceptron.cm.cluster>
2019-04-29 19:49:19 -0400

    fix bugs

--- src/spark/spark_v2.py
+++ src/spark/spark_v2.py
@@ -4,20 +4,24 @@ findspark.init("/home/jingguaz/jingguang/bin/spark")
 import pyspark
 from pyspark import SparkConf, SparkContext
 
+import math
+
+
 conf = SparkConf()
 #conf.set("spark.executor.memory", "6G")
 #conf.set("spark.driver.memory", "2G")
 #conf.set("spark.executor.cores", "4")
 #conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
-num_workers = 10
-num_factors = 100
+num_workers = 16
+num_factors = 5#100
 num_iterations = 50
 beta = 0.8
-lamda = 1.0
+lamda = 1.0#0.02#1.0
 samplingCnt = 100
 
 dataset_type = "10M"
+dataset_type = "100k"
 inputV_filepath = None
 test_inputV_filepath = None
 
@@ -37,9 +41,6 @@ conf.setMaster('local[{}]'.format(num_workers))
 sc = SparkContext(conf=conf)
 
 
-import numpy as np
-import math
-
 def parsing(line):
     if dataset_type == "100k":
         line = line.split("\t")
@@ -55,15 +56,17 @@ numMovies = lines.max(lambda x :x[1])[1] + 1
 blockUsers = int(math.ceil(numUsers/num_workers))
 blockMovies = int(math.ceil(numMovies/num_workers))
 totalData = lines.count()
+averRate = lines.map(lambda x: x[2]).reduce(lambda x, y: x+y)
+scale = (averRate / totalData / num_factors) ** (1./2)
 
 #H = lines.map(lambda x: (x[1], np.random.rand(num_factors,1)))
 #W = lines.map(lambda x: (x[0], np.random.rand(1, num_factors)))
 
 #(Movie, (Nj, vec))
-H = lines.map(lambda x: (x[1], 1)).reduceByKey(lambda x, y: x+y).map(lambda x: (x[0], (x[1], np.random.normal(0, 0.5, (num_factors, 1)))))
+H = lines.map(lambda x: (x[1], 1)).reduceByKey(lambda x, y: x+y).map(lambda x: (x[0], (x[1], np.random.normal(0, 0.5*scale, (num_factors, 1)))))
 
 #(User, (Ni, vec))
-W = lines.map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: x+y).map(lambda x: (x[0], (x[1], np.random.normal(0, 0.5, (1, num_factors)))))
+W = lines.map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: x+y).map(lambda x: (x[0], (x[1], np.random.normal(0, 0.5*scale, (1, num_factors)))))
 
                                                     
 V = lines.keyBy(lambda x: int(x[0] / blockUsers)).partitionBy(num_workers).persist() #(node, (user, movie, rate))
@@ -74,8 +77,9 @@ test_totalData = test_lines.count()
 test_V = test_lines.keyBy(lambda x: int(x[0] / blockUsers)).partitionBy(num_workers).persist() #(node, (user, movie, rate))
 
 
-
-def updateParameter(tup, n):
+dummy = 1
+print(dummy)
+def updateParameter1(tup, n):
     #tup = list(tup_iter)#(tup_iter.collect()) 
     data = tup[1]
     V_sub, W_sub, H_sub = data[0], data[1], data[2]
@@ -114,8 +118,9 @@ def updateParameter(tup, n):
             tmp = np.dot(Wi, Hj)
             inner = (r - tmp)
             #err += inner
-
-            lr = (100 + n + cnt) ** (-beta)
+            lr = 0.0001
+            #lr = (100 + 10*n) ** (-beta)
+            #lr = (100 + n + cnt) ** (-beta)
             Wi -= lr * (-2.0*(inner)*Hj + 
                                      2.0 * lamda * Wi.T / Ni).T # W_sub.shape[1]).T
             Hj -= lr * (-2.0*(inner)*Wi.T +
@@ -161,15 +166,21 @@ def computeError(tup):
     err_sub = 0
    
     u, m, r = item[0], item[1], item[2]
-    Ni, Wi = W_broad.value[u]
-    Nj, Hj = H_broad.value[m]
+    
+    if u not in W_broad.value or m not in H_broad.value:
+        Wi = np.random.normal(0, 0.5*scale, (num_factors, 1))
+        Hj = np.random.normal(0, 0.5*scale, (1, num_factors))
+    else:
+        Ni, Wi = W_broad.value[u]
+        Nj, Hj = H_broad.value[m]
     tmp = np.dot(Wi, Hj)[0][0]
     inner = r - tmp
     #err_sub += inner#(inner**2)
     err_sub += inner**2
-    return err_sub
+    return err_sub ** (1./2)
 
 
+import time
 
 #%pdb
 iter_n = 100
@@ -182,6 +193,8 @@ W_broad = None
 def train_one(itr):
     global H, W, H_broad, W_broad
     global iter_n
+    
+    t_begin = time.process_time()
     strata = np.random.permutation(num_workers)
     V_subs = V.filter(lambda x: strata[int(x[1][1]/blockMovies)] == x[0]) #[node, (user, movie,  vec)]
     H_subs = H.keyBy( lambda x: strata[int(x[0] / blockMovies)]).partitionBy(num_workers) #[node, (movie, (N, vec))] 
@@ -190,7 +203,7 @@ def train_one(itr):
     
     argsInput = V_subs.groupWith(W_subs, H_subs).partitionBy(num_workers)
     
-    argsOutput = argsInput.map(lambda x: updateParameter(x, iter_n), preservesPartitioning=True).persist() #[node, (Wdict, Hdict)]
+    argsOutput = argsInput.map(lambda x: updateParameter1(x, iter_n), preservesPartitioning=True).persist() #[node, (Wdict, Hdict)]
 
     #W_subs = argsOutput.map(lambda x: (x[0], x[1][0]))
     #H_subs = argsOutput.map(lambda x: (x[0], x[1][1]))
@@ -201,21 +214,38 @@ def train_one(itr):
     H = argsOutput.flatMap(lambda x: x[1]).persist()
     #print(W.collect())
     
+    t_mid = time.process_time() 
+    
     err = argsOutput.map(lambda x: x[2])
     #print(err.collect())
     err = err.reduce(lambda x, y: x+y) #for map
     #err_re = argsOutput.map(lambda x: x[3]).reduce(lambda x, y: x+y)
     
+    t_mid1 = time.process_time() 
+    
     W_broad = sc.broadcast(W.collectAsMap())
     H_broad = sc.broadcast(H.collectAsMap())
+    
+    t_mid2 = time.process_time() 
     #print(V.first()) #(0, [0, 0, 5.0])
     err_total = V.map(computeError).reduce(lambda x,y: x+y)
+    
+    t_mid3 = time.process_time() 
+    
     err_test = test_V.map(computeError).reduce(lambda x,y: x+y)
+    
+    t_end = time.process_time() 
     
     #iter_n = iter_n + totalData
     iter_n = iter_n + itr
     #iter_n = iter_n + num_workers * 100
     #W_subs.collect()
-    print("Total Loss at Iter {}, lr = {}:".format(itr, (100 + iter_n) ** (-beta)), err, err_total/totalData, err_test/test_totalData)
+   
+    print("Total Loss at Iter {}, lr = {}:".format(itr, (100 + iter_n) ** (-beta)), err[0][0], err_total/totalData, err_test/test_totalData)
+    print("Pass Time: total {0:.4f}, {1:.4f}".format(t_mid - t_begin, (t_end - t_mid) ) )
+    #print(t_mid1 - t_mid, t_mid2 - t_mid1, t_mid3 - t_mid2, t_end - t_mid3)
     #print("Total Loss at Iter {}, lr = {}:".format(itr, (100 + iter_n) ** (-beta)), err, err_total/totalData)
 
+    
+for i in range(100):
+    train_one(i)
